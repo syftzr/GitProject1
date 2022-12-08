aba9e50be21ac983546d8ff019292b484a65a37e
Seyfi Tozar <syftozarmail.com>
2022-12-08 16:11:41 +0300

    added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -3,5 +3,7 @@ package work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("work1 started");
+        System.out.println("created Git repository");
+
     }
 }
