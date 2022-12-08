3801feb7321fcddaae66e9b53aa028b3f6d8ca89
Seyfi Tozar <syftozarmail.com>
2022-12-08 16:59:41 +0300

    added more hello feature

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
         System.out.println("work1 started");
         System.out.println("created Git repository");
         System.out.println("made few commits");
+        System.out.println("Local Repo is already published to Github");
 
     }
 }
